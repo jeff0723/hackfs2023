8b9a935b0592dbcec9e8fd017411b58656e46f03
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-06-13 16:21:32 +0800

    add redux

---
+++ package/frontend/src/state/updater.ts
@@ -0,0 +1,39 @@
+import { HACKFS_AI_NFT_ABI } from "@/constant/abis";
+import { HACKFS_AI_NFT_ADDRESS } from "@/constant/addresses";
+import { useEffect } from "react";
+import { useAccount, useContractRead, useWalletClient } from "wagmi";
+import { useAppDispatch } from "./hook";
+import { updateCurrentAgent } from "./application";
+
+
+type Props = {};
+
+const ApplicationUpdater = (props: Props) => {
+    const dispatch = useAppDispatch()
+    const { address } = useAccount()
+    const { data, isError, isLoading } = useContractRead({
+        address: HACKFS_AI_NFT_ADDRESS,
+        abi: HACKFS_AI_NFT_ABI,
+        functionName: 'agentURI',
+        args: [1340],
+    })
+    useEffect(() => {
+        if (data && address) {
+            const agentURI = data as string
+            fetch(agentURI).then((res) => res.json()).then((data) => {
+                dispatch(updateCurrentAgent({
+                    currentAgent: data
+                }))
+            })
+        }
+        if (!address) {
+            dispatch(updateCurrentAgent({
+                currentAgent: undefined
+            }))
+        }
+
+    }, [data, address])
+    return null;
+};
+
+export default ApplicationUpdater;
