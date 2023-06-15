4c187ed7e49674793bc7b9ff505838734db81159
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-06-15 21:15:26 +0800

    add settings

---
+++ package/frontend/src/pages/settings/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SettingsPage = (props: Props) => {
+    return (
+        <div className='px-[20px] flex'>Settings</div>
+    )
+}
+
+export default SettingsPage
